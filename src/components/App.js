af99eec826dee70b89e824f7076c21ef117d0c84
samidejen <bold.3453a@gmail.com>
2025-05-03 05:41:52 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -100,7 +100,7 @@ export default function App() {
   );
 
   useEffect(function () {
-    fetch("https://vinayak9669.github.io/React_quiz_api/questions.json")
+    fetch("https://github.com/samidejen/React_quiz_api/blob/main/questions.json")
       .then((res) => res.json())
       .then((data) =>
         dispatch({
